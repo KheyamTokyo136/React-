4afc3c414d2d1b33637f76e511d1291521e9b576
Kheyam Khan <khankheyam41@gmail.com>
2026-09-15 18:00:31 +0100

    Day 2: 02-components

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+      <h1>Kheyam khan</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      
+    </div>
+    </div>
+  )
+}
+
+export default Card
